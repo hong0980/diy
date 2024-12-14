91c454a3bc3a54da36c1687c9f0767ce06ebc7e4
hong0980 <hong0980@qq.com>
2024-12-15 00:47:54 +0800

    update@2024-12-15-00-47

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -72,16 +72,13 @@ git_diff() {
     fi
 
     for i in "$@"; do
-        echo -e "$(color cy '制作补丁：') $i\c "
-        pwd
-        # git cat-file commit origin/${REPO_BRANCH}:"$i" &>/dev/null && \
-        # git diff --quiet "$i" || \
-        git diff -- "$i" > $GITHUB_WORKSPACE/firmware/${REPO_BRANCH}-${i##*/}.patch || echo -e "$(color cy '制作补丁：') $i 失败！"
+        git cat-file commit origin/${REPO_BRANCH}:"$i" &>/dev/null &&
+        git diff -- "$i" > $GITHUB_WORKSPACE/firmware/${REPO_BRANCH}-${i##*/}.patch
     done
-    pwd
 
     [[ -n $path ]] && safe_popd
-    ls -a ../firmware
+    ls -la ../firmware
+    find ../firmware -type f -empty -delete
 }
 
 git_apply() {
@@ -701,10 +698,13 @@ sed -i '/bridge\|vssr\|deluge/d' .config
 	CONFIG_PACKAGE_luci-app-upnp=y
 	CONFIG_PACKAGE_luci-app-wizard=y
 	CONFIG_PACKAGE_luci-app-poweroff=y
+	CONFIG_PACKAGE_luci-app-passwall2=y
 	EOF
-    git_apply ../firmware/${REPO_BRANCH}-luci-app-diskman.patch feeds/luci
-    git_apply ../firmware/${REPO_BRANCH}-luci-app-dockerman.patch feeds/luci
-    clone_dir sbwml/openwrt_helloworld luci-app-ssr-plus luci-app-passwall2 shadowsocks-libev shadow-tls
+    create_directory "feeds/luci/applications/luci-app-passwall2"
+    git_apply $GITHUB_WORKSPACE/firmware/${REPO_BRANCH}-luci-app-diskman.patch feeds/luci
+    git_apply $GITHUB_WORKSPACE/firmware/${REPO_BRANCH}-luci-app-dockerman.patch feeds/luci
+    clone_dir sbwml/openwrt_helloworld luci-app-ssr-plus luci-app-passwall2 shadowsocks-libev \
+    shadowsocksr-libev shadow-tls pdnsd-alt ssocks v2ray-geoview
     clone_dir hong0980/build luci-app-timedtask luci-app-tinynote luci-app-wizard luci-app-poweroff
     sed -i '/n) ipad/s/".*"/"'"$IP"'"/' $config_generate
     sed -i "/DISTRIB_DESCRIPTION/ {s/'$/-$SOURCE_NAME-$(TZ=UTC-8 date +%Y年%m月%d日)'/}" package/*/*/*/openwrt_release
