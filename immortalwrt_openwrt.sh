a8bc3210d811e26a374cc6bdcfe1aa276ba59e0f
hong0980 <hong0980@qq.com>
2025-04-13 22:55:49 +0800

    update@2025-04-13-22-55

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -394,7 +394,7 @@ if [[ $REPO_BRANCH =~ master|23|24 ]]; then
 	fi
 	clone_dir nikkinikki-org/OpenWrt-nikki nikki luci-app-nikki
 	# git_diff "feeds/luci/collections/luci-lib-docker" "feeds/luci/applications/luci-app-dockerman"
-	clone_dir fw876/helloworld luci-app-ssr-plus shadow-tls shadowsocks-libev shadowsocksr-libev mosdns lua-neturl
+	clone_dir fw876/helloworld luci-app-ssr-plus shadow-tls shadowsocks-libev shadowsocksr-libev mosdns lua-neturl dns2socks-rust
 	[[ $TARGET_DEVICE =~ k2p ]] || \
 		addpackage "autosamba luci-app-diskman luci-app-qbittorrent luci-app-poweroff luci-app-pushbot luci-app-dockerman luci-app-softwarecenter luci-app-usb-printer luci-app-nikki"
 
