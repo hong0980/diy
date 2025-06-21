d38391c1cd7718adf0b791014b882ad1bc089e8b
hong0980 <hong0980@qq.com>
2025-06-21 22:53:02 +0800

    update@2025-06-21-22-53

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -386,7 +386,7 @@ clone_dir hong0980/build ddnsto luci-app-ddnsto luci-app-diskman luci-app-docker
 	luci-app-timedtask luci-app-tinynote luci-app-wizard luci-app-easymesh luci-lib-docker \
 	aria2 luci-app-aria2 sunpanel lsscsi axel luci-app-taskplan luci-app-watchdog \
 	deluge luci-app-deluge python-pyxdg python-rencode python-setproctitle \
-	libtorrent-rasterbar
+	libtorrent-rasterbar python-mako
 clone_dir openwrt/packages docker dockerd containerd docker-compose runc golang nlbwmon
 clone_dir sirpdboy/luci-app-partexp luci-app-partexp
 clone_dir sirpdboy/luci-app-ddns-go ddns-go luci-app-ddns-go
@@ -473,8 +473,6 @@ sed -Ei \
 
 [ -f feeds/luci/applications/luci-app-transmission/Makefile ] && \
 	sed -i 's/transmission-daemon/transmission-daemon +transmission-web-control/' feeds/luci/applications/luci-app-transmission/Makefile
-[ -f feeds/packages/lang/python/python-mako/Makefile ] && \
-	sed -i '/^BUILDONLY:=1/s/^/#/' feeds/packages/lang/python/python-mako/Makefile
 
 find {package/A,feeds/luci/applications}/luci-app*/po -type d 2>/dev/null | while read p; do
 	if [[ -d $p/zh-cn && ! -e $p/zh_Hans ]]; then
