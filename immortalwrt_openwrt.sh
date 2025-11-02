455a3722bf29bb5f2c0f3d5c611a94722de6991a
hong0980 <hong0980@qq.com>
2025-11-02 08:57:57 +0800

    update@2025-11-02-08-57

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -416,7 +416,8 @@ if [[ $REPO_BRANCH =~ master|23|24 ]]; then
 		sed -i '/^define Py3Build\/Install\/Default/a \
 		\t# Clean duplicated metadata and license files before python -m installer\n\t$(FIND) $(PKG_INSTALL_DIR) -type f \\( -name AUTHORS -o -name LICENSE -o -name COPYING \\) -delete || true\n\t$(FIND) $(PKG_INSTALL_DIR) -type f -path "*/.dist-info/licenses/*" -delete || true
 		' feeds/packages/lang/python/python3-package.mk
-		sed -i '/deluge/d' .config
+		sed -i 's#"\$(PYTHON3_PKG_BUILD_DIR)"/openwrt-build/\$(PYTHON3_PKG_WHEEL_NAME)-\$(PYTHON3_PKG_WHEEL_VERSION)-\*.whl#$(PYTHON3_PKG_BUILD_DIR)/openwrt-build/*\$(PYTHON3_PKG_WHEEL_VERSION)*.whl#' feeds/packages/lang/python/python3-package.mk
+
 	else
 		sed -i "s/ImmortalWrt/OpenWrt/g" {$config_generate,include/version.mk} || true
 	fi
