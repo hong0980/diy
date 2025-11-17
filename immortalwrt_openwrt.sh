808a70a3d9206b3d0ca88c6c1bf64ed58543c82e
hong0980 <hong0980@qq.com>
2025-11-17 18:09:00 +0800

    update@2025-11-17-18-09

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -501,7 +501,7 @@ sed -Ei '{
 	sed -i 's/transmission-daemon/transmission-daemon +transmission-web-control/' feeds/luci/applications/luci-app-transmission/Makefile
 
 [[ "$TARGET_DEVICE" =~ armvirt ]] && sed -i '/qbittorrent/d' .config
-# [[ $REPO_BRANCH =~ master ]] && sed -i '/deluge/d' .config
+[[ $REPO_BRANCH =~ master ]] && sed -i '/deluge/d' .config
 echo -e "$(color cy '更新配置....')\c"
 begin_time=$(date '+%H:%M:%S')
 make defconfig 1>/dev/null 2>&1
