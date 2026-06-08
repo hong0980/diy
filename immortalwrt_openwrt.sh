b06ec0be9a7b8509b35e94ffe7dff6d47d02b92c
hong0980 <hong0980@qq.com>
2026-06-08 11:47:32 +0800

    update@2026-06-08-11-47

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -476,7 +476,7 @@ clone_dir sbwml/openwrt_helloworld shadowsocks-rust
 clone_dir vernesong/OpenClash luci-app-openclash
 clone_dir Openwrt-Passwall/openwrt-passwall luci-app-passwall
 clone_dir Openwrt-Passwall/openwrt-passwall2 luci-app-passwall2
-clone_dir Openwrt-Passwall/openwrt-passwall-packages chinadns-ng geoview trojan-plus sing-box xray-core simple-obfs shadowsocks-libev
+clone_dir Openwrt-Passwall/openwrt-passwall-packages chinadns-ng geoview trojan-plus xray-core simple-obfs shadowsocks-libev
 
 # clone_dir kiddin9/kwrt-packages ddns-go gecoosac lua-maxminddb \
 # 		luci-app-advancedplus luci-app-arpbind luci-app-ddns-go luci-app-gecoosac \
