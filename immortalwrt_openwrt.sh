1045b1b66597f4706553b7382845f8c30c87b30d
hong0980 <hong0980@qq.com>
2026-06-16 17:08:40 +0800

    update@2026-06-16-17-08

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -371,7 +371,7 @@ set_config (){
 		luci-app-nlbwmon luci-app-bypass luci-app-openclash luci-app-passwall2 luci-app-tinynote luci-app-nikki \
 		luci-app-uhttpd luci-app-usb-printer luci-app-dockerman luci-app-softwarecenter luci-app-ddns-go \
 		luci-app-qbittorrent luci-app-deluge luci-app-transmission luci-app-aria2 webui-aria2 \
-		luci-app-miaplus luci-app-watchdog luci-app-fchomo luci-app-daed
+		luci-app-miaplus luci-app-watchdog luci-app-fchomo
 
 	add_package autocore opkg luci-app-arpbind luci-app-ssr-plus luci-app-passwall \
 				luci-app-upnp luci-app-ttyd luci-app-taskplan luci-app-wizard luci-app-tinynote-js \
